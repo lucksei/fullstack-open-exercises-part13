0d5bd1dbca17d3e3bd92b8c9f1564accb2908bb4
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-10-25 16:47:17 -0300

    exercise 13.7 done -- error middleware implemented, using express 5 so no need for express-async-errors i think

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -2,14 +2,16 @@
 const express = require('express')
 // Controllers
 const blogsRouter = require('./controllers/blogs')
+// Middleware
+const { errorHandler } = require('./util/middlewares')
 // Utils
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const app = express()
 app.use(express.json())
-
 app.use('/api/blogs', blogsRouter)
+app.use(errorHandler)
 
 const start = async () => {
   await connectToDatabase()
