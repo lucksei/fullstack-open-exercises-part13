7bdf3b5be8fe6ea34d11392c8a9798126f06fbbb
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-10-26 15:48:58 -0300

    exercise 13.8 done, working state for users

--- bloglist-backend/controllers/users.js
+++ bloglist-backend/controllers/users.js
@@ -24,4 +24,16 @@ router.get('/:id', userFinder, async (req, res) => {
   return res.status(200).json(req.user);
 })
 
+router.put('/:username', async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username
+    }
+  })
+  if (!user) {
+    throw new NotFoundError('Error, user not found')
+  }
+  await user.update({ username: req.body.username })
+  return res.status(200).json(user)
+})
 module.exports = router
