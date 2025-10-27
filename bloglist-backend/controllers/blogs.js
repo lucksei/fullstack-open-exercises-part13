c16ab39f6bb37ae679f11f4d27538d735b18fafa
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-10-26 22:14:19 -0300

    exercise 13.13 done -- search parameters implemented for GET /api/blogs

--- bloglist-backend/controllers/blogs.js
+++ bloglist-backend/controllers/blogs.js
@@ -1,15 +1,24 @@
 const router = require('express').Router()
+const { Op } = require('sequelize')
 const { Blog, User } = require('../models')
 const { NotFoundError, UnauthorizedError } = require('../util/errors')
 const { tokenExtractor } = require('../util/middlewares')
 
 router.get('/', async (req, res) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = { [Op.iLike]: `%${req.query.search}%` }
+  }
+
   const blogs = await Blog.findAll({
     include: {
       model: User,
     },
     attributes: { exclude: ['userId'] },
+    where
   });
+
   return res.status(200).json(blogs)
 })
 
